95dbc2adfea46448523aa701450e004fb2c576eb
OlgaAgafonova <AndersenPractice@gmail.com>
2016-07-07 11:53:46 +0300

    Added new fields for User

--- src/main/java/stack/model/User.java
+++ src/main/java/stack/model/User.java
@@ -1,9 +1,30 @@
 package stack.model;
 
+import org.hibernate.validator.constraints.Email;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
-    private int id;
+    private Integer id;
+
+    @NotNull
+    @Size(min = 5, max = 16, message = "{username.size}")
     private String login;
+
+    @NotNull
+    @Size(min = 5, max = 25, message = "{password.size}")
     private String password;
+
+    @NotNull
+    @Email
+    private String email;
+
+    @Size(min = 2, max = 16, message = "{firstName.size}")
+    private String firstName;
+
+    @Size(min = 2, max = 16, message = "{lastName.size}")
+    private String lastName;
 
     public User() {
     }
@@ -17,6 +38,14 @@ public class User {
         this.id = id;
         this.login = login;
         this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getLogin() {
@@ -35,11 +64,39 @@ public class User {
         this.password = password;
     }
 
-    public int getId() {
-        return id;
+    public String getEmail() {
+        return email;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
     }
 }
